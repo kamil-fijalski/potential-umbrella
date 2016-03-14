edc8d95f3f443f71735356c0264eac5ba01f5c35
seeksoul <kamil.fijalski@outlook.com>
2016-03-14 22:30:16 +0100

    Final app version + sending email

--- JustJava/app/src/main/java/com/seeksoul/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/seeksoul/justjava/MainActivity.java
@@ -1,5 +1,7 @@
 package com.seeksoul.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -9,7 +11,8 @@ import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity
 {
-    int numberOfCoffees = 0;
+    //simply the number of coffee
+    int numberOfCoffees = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -35,6 +38,7 @@ public class MainActivity extends ActionBarActivity
         displayQuantity(numberOfCoffees);
     }
 
+    //main price engine which compute prize of order
     private int calculatePrice(int quantity)
     {
         if (CheckCream() == true && CheckChoco() == true)
@@ -55,6 +59,7 @@ public class MainActivity extends ActionBarActivity
         }
     }
 
+    //method used by Incr. and Decr. to display the number of coffee
     private void displayQuantity(int number)
     {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
@@ -67,34 +72,63 @@ public class MainActivity extends ActionBarActivity
         summaryOrder.setText(message);
     }
 
-    public void submitOrder(View view)
-    {
+    public void submitOrder(View view) {
         String priceMessage = summaryOrder(numberOfCoffees);
         displayMessage(priceMessage);
+
+//        Intent location = new Intent(Intent.ACTION_VIEW);
+//        location.setData(Uri.parse("geo: 52.25, 20.93"));
+//        if (location.resolveActivity(getPackageManager()) != null)
+//        {
+//            startActivity(location);
+//        }
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setData(Uri.parse("mailto:"));
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, "");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Order for " + InsertName());
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivity(intent);
+        }
     }
 
+    //display the final summary of order include quantity, prize and owner of order
     private String summaryOrder(int quantity)
     {
 
         String summary = "Name: " + InsertName() + "\nQuantity: " + quantity;
+        if (CheckCream())
+        {
+            summary += "\nMmmmmm... with delicious cream!";
+        }
+        if (CheckChoco())
+        {
+            summary += "\nWith fantastic swiss chocolate!";
+        }
         summary += "\nPrice: " + calculatePrice(quantity) + "zł";
         summary += "\nThank You!";
 
         return summary;
     }
 
+    //describe the state of additional cream to coffee
     private boolean CheckCream()
     {
         CheckBox cream = (CheckBox) findViewById(R.id.state_of_cream);
         return cream.isChecked();
     }
 
+    //describe the state of additional chocolate to coffee
     private boolean CheckChoco()
     {
         CheckBox choco = (CheckBox) findViewById(R.id.state_of_chocolate);
         return choco.isChecked();
     }
 
+    //pass the information about owner of order
     private String InsertName()
     {
         EditText Name = (EditText) findViewById(R.id.name_field);
