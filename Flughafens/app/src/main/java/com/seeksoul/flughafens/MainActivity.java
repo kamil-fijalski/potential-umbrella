62a3015af31c5d8336aa43b71d64813313bef48c
seeksoul <kamil.fijalski@outlook.com>
2016-03-17 00:09:17 +0100

    Final app version

--- Flughafens/app/src/main/java/com/seeksoul/flughafens/MainActivity.java
+++ Flughafens/app/src/main/java/com/seeksoul/flughafens/MainActivity.java
@@ -4,43 +4,77 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-   @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    int id;
+    Intent goToActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void GoOkecie(View view)
-    {
-        Intent intent  = new Intent(this, OkecieActivity.class);
-        startActivity(intent);
+    public void GoOkecie(View view) {
+        id = 1;
+        goToActivity = new Intent(MainActivity.this, AirportActivity.class);
+        goToActivity.putExtra("id", "" + id);
+
+        Button b = (Button) view;
+        String buttonText = b.getText().toString();
+
+        Toast.makeText(MainActivity.this, buttonText, Toast.LENGTH_LONG).show();
+        startActivity(goToActivity);
     }
 
-    public void GoFrankfurt(View view)
-    {
-        Intent intent = new Intent(this, FrankfurtActivity.class);
-        startActivity(intent);
+    public void GoFrankfurt(View view) {
+        id = 2;
+        goToActivity = new Intent(MainActivity.this, AirportActivity.class);
+        goToActivity.putExtra("id", "" + id);
+
+        Button b = (Button) view;
+        String buttonText = b.getText().toString();
+
+        Toast.makeText(MainActivity.this, buttonText, Toast.LENGTH_LONG).show();
+        startActivity(goToActivity);
     }
 
-    public void GoLondon(View view)
-    {
-        Intent intent = new Intent(this, LondonActivity.class);
-        startActivity(intent);
+    public void GoLondon(View view) {
+        id = 3;
+        goToActivity = new Intent(MainActivity.this, AirportActivity.class);
+        goToActivity.putExtra("id", "" + id);
+
+        Button b = (Button) view;
+        String buttonText = b.getText().toString();
+
+        Toast.makeText(MainActivity.this, buttonText, Toast.LENGTH_LONG).show();
+        startActivity(goToActivity);
     }
 
-    public void GoDC(View view)
-    {
-        Intent intent = new Intent(this, DCActivity.class);
-        startActivity(intent);
+    public void GoDC(View view) {
+        id = 4;
+        goToActivity = new Intent(MainActivity.this, AirportActivity.class);
+        goToActivity.putExtra("id", "" + id);
+
+        Button b = (Button) view;
+        String buttonText = b.getText().toString();
+
+        Toast.makeText(MainActivity.this, buttonText, Toast.LENGTH_LONG).show();
+        startActivity(goToActivity);
     }
 
-    public void GoParis(View view)
-    {
-        Intent intent = new Intent(this, ParisActivity.class);
-        startActivity(intent);
+    public void GoParis(View view) {
+        id = 5;
+        goToActivity = new Intent(MainActivity.this, AirportActivity.class);
+        goToActivity.putExtra("id", "" + id);
+
+        Button b = (Button) view;
+        String buttonText = b.getText().toString();
+
+        Toast.makeText(MainActivity.this, buttonText, Toast.LENGTH_LONG).show();
+        startActivity(goToActivity);
     }
 }
